9121e8a1749a8f0cec354c654a3329ae8ff63ca1
joycelin12 <joycelin12@hotmail.com>
2022-02-25 18:45:31 -0500

    Add state

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 export default class MainContent extends Component {
+  state = { pageTitle: "Customers", customersCOunt: 5 };
+
   render() {
-    return <div>Hello from Main Content</div>;
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.pageTitle}
+          <span className="badge bg-secondary m-2">
+            {this.state.customersCOunt}
+          </span>
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
+        </h4>
+      </div>
+    );
+  }
+
+  //Executes when the user clicks on Refresh button
+  onRefreshClick() {
+    console.log("Refresh clicked");
   }
 }
